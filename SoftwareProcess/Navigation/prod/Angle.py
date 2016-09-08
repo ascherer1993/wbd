f1c7dd9335710e343c5d570cc341d85e2a3032e8
ascherer1993 <ascherer1993@gmail.com>
2016-09-08 01:04:03 -0500

    second check in

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,25 +1,36 @@
 class Angle():
     def __init__(self):
-        #self.angle = ...       set to 0 degrees 0 minutes
+        self.angle = 0
         pass
     
     def setDegrees(self, degrees):
+        self.angle = degrees
+        #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
     
-    def setDegreesAndMinutes(self, degrees):
+    def setDegreesAndMinutes(self, angleString):
+        # split on 'd', 45d10.1
+        #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
     
     def add(self, angle):
+        #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
     
     def subtract(self, angle):
+        #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
     
     def compare(self, angle):
+        # -1 if less than parameter
+        # 0 if equal
+        # 1 if greater than parameter
         pass
     
     def getString(self):
+        #returns xdy.y
         pass
     
     def getDegrees(self):
+        #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
