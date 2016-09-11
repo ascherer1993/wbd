3aac2ab1fd1a4e9e5a0262c1d280d2d926fe83a5
ascherer1993 <ascherer1993@gmail.com>
2016-09-11 15:16:16 -0500

    changes to methods and creation of tests. starting to add regex

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,18 +1,24 @@
 from test.test_typechecks import Integer
 class Angle():
     def __init__(self):
-        self.angle = 0
+        self.angle = 0.0
         pass
     
     def setDegrees(self, degrees):
         if not isinstance(degrees, float) and not isinstance(degrees, int) :
             raise ValueError("Angle.setDegrees:  The parameter provided was not a integer or float")
-        self.angle = degrees % 360
+        self.angle = round(degrees % 360, 1)
         return self.angle
         #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
     
     def setDegreesAndMinutes(self, angleString):
+        
+        regex = "^[-0-9]+d[0-9]+\.[0-9]"
+        
+        
+        
+        
         if 'd' in angleString:
             newString = angleString.split("d")
             if (len(newString) != 2 and not newString[1].isdigit()) :
@@ -39,28 +45,47 @@ class Angle():
         #pass
     
     def add(self, angle):
+        if not isinstance(angle, Angle) :
+            return ValueError("Angle.compare:  The parameter you have provided is not an instance of the Angle class.")
+        self.angle = (self.angle + angle.angle) % 360
+        return self.angle
         #returns angle as degrees and portions of degrees as a single floating point number mod 360
         pass
     
     def subtract(self, angle):
-        #returns angle as degrees and portions of degrees as a single floating point number mod 360
+        if not isinstance(angle, Angle) :
+            return ValueError("Angle.compare:  The parameter you have provided is not an instance of the Angle class.")
+        self.angle = (self.angle - angle.angle) % 360
+        return self.angle
         pass
     
     def compare(self, angle):
+        if not isinstance(angle, Angle) :
+            return ValueError("Angle.compare:  The parameter you have provided is not an instance of the Angle class.")
+        try :
+            if self.angle > angle.angle :
+                return 1
+            elif self.angle == angle.angle :
+                return 0
+            elif self.angle < angle.angle :
+                return -1
+        except :
+            return ValueError("Angle.compare:  An error has occured")
+        
         # -1 if less than parameter
         # 0 if equal
         # 1 if greater than parameter
         pass
     
     def getString(self):
-        if '.' in self.angle :
-            degreesArray = self.angle.split('.')
+        if isinstance(self.angle, float) :
+            degreesArray = str(self.angle).split('.')
             
-            decimalInMinuteForm = degreesArray[1] * 60
-            self.angle = degreesArray[0] + decimalInMinuteForm
+            decimalInMinuteForm = float('.' + degreesArray[1]) * 60
+            self.angle = str(degreesArray[0]) + "d" + str(round(decimalInMinuteForm, 1))
             return self.angle
         else :
-            return self.angle
+            return str(self.angle) + "d0.0"
         
         pass
     
--- SoftwareProcess/Navigation/test/AngleTest.py
+++ SoftwareProcess/Navigation/test/AngleTest.py
@@ -5,16 +5,25 @@ Created on Sep 11, 2016
 '''
 import unittest
 import Navigation.prod.Angle as angle 
+from test.test_typechecks import Integer
 
 class Test(unittest.TestCase):
 
     
 
     def setUp(self):
+        self.angleObject1 = angle.Angle()
+        self.angleObject2 = angle.Angle()
+        self.angleObject3 = angle.Angle()
+        self.angleObject2.angle = 20
+        self.angleObject3.angle = -40
         pass
 
 
     def tearDown(self):
+        self.angleObject1 = None
+        self.angleObject2 = None
+        self.angleObject3 = None
         pass
 
 
@@ -22,33 +31,92 @@ class Test(unittest.TestCase):
         self.assertIsInstance(angle.Angle(), angle.Angle)
         pass
 
-    def test101_010_setDegrees(self):
-        angleObject = angle.Angle()
-#         try:
-#             angleObject.setDegrees("2d5.1")
-#             print(angleObject.getDegrees())
-#         except ValueError as e:
-#             print(e)
-        self.assert_(angleObject.setDegrees(25) == 25, "Did not set angle correctly")
-        self.assert_(angleObject.setDegrees(25.1) == 25.1, "Did not set angle correctly")
-        self.assert_(angleObject.setDegrees(375) == 15, "Did not set angle correctly")
-        self.assert_(angleObject.setDegrees(-15) == 345, "Did not set angle correctly")
+    def test101_010_setDegreesSuccess(self):
+        #Type checking
+        self.assert_(isinstance(self.angleObject1.setDegrees(10), float))
+        
+        #Value checking
+        self.assert_(self.angleObject1.setDegrees(25) == 25, "Did not set angle correctly")
+        self.assert_(self.angleObject1.setDegrees(25.1) == 25.1, "Did not set angle correctly")
+        self.assert_(self.angleObject1.setDegrees(375) == 15, "Did not set angle correctly")
+        self.assert_(self.angleObject1.setDegrees(-15) == 345, "Did not set angle correctly")
 #         print(angleObject.getDegrees())
         pass
     
-    def test101_020_setDegreesStringCheck(self):
-        angleObject = angle.Angle()
-        self.assertRaises(ValueError, angleObject.setDegrees, "25")
+    def test101_020_setDegreesFail(self):
+        self.assertRaises(ValueError, self.angleObject1.setDegrees, "25")
         pass
-
-    def test102_010_setDegreesAndMinutes(self):
-        angleObject = angle.Angle()
-        self.assertRaises(ValueError, angleObject.setDegreesAndMinutes, "25d10y")
+ 
+    def test102_010_setDegreesAndMinutesSuccess(self):
+        self.assertRaises(ValueError, self.angleObject1.setDegreesAndMinutes, "25d10y")
         pass
     
-    def test107_010_getDegreesTest(self):
-        angleObject = angle.Angle()
-        self.assertRaises(ValueError, angleObject.setDegreesAndMinutes, "25d10y")
+    def test102_020_setDegreesAndMinutesFail(self):
+        self.assertRaises(ValueError, self.angleObject1.setDegreesAndMinutes, "25d10y")
+        pass
+    
+    def test103_010_addSuccess(self):
+        self.assert_(self.angleObject1.add(self.angleObject2) == 20, "Did not set angle correctly")
+        pass
+    
+    def test103_010_addFail(self):
+        self.assertRaises(ValueError, self.angleObject1.setDegreesAndMinutes, "25d10y")
+        pass
+    
+    def test104_010_subtractSuccess(self):
+        self.assert_(self.angleObject1.setDegrees(-15) == 345, "Did not set angle correctly")
+        pass
+    
+    def test104_020_subtractFail(self):
+        self.assertRaises(ValueError, self.angleObject1.setDegreesAndMinutes, "25d10y")
+        pass
+    
+    def test105_010_getStringTestSuccess(self):
+        self.assert_(self.angleObject1.setDegrees(-15) == 345, "Did not set angle correctly")
+        
+        # Setting angle without using the method is for testing purposes, as those methods are checked elsewhere
+        self.angleObject1.angle = 25
+        self.assert_(self.angleObject1.getString() == "25d0.0", "Your method must be broken")
+        
+        self.angleObject1.angle = 25.8        
+        self.assert_(self.angleObject1.getString() == "25d48.0", "Your method must be broken")
+        
+        pass
+    def test105_020_getStringTestFail(self):
+        self.assert_(self.angleObject1.getString() == "0d0.0", "Your method must be broken")
+        
+        # Setting angle without using the method is for testing purposes, as those methods are checked elsewhere
+        self.angleObject1.angle = 25
+        self.assert_(self.angleObject1.getString() == "25d0.0", "Your method must be broken")
+        
+        self.angleObject1.angle = 25.8        
+        self.assert_(self.angleObject1.getString() == "25d48.0", "Your method must be broken")
+        
+        pass
+
+    
+    def test106_010_getDegreesTestSuccess(self):
+        self.assert_(self.angleObject1.setDegrees(-15) == 345, "Did not set angle correctly")
+        
+        # Setting angle without using the method is for testing purposes, as those methods are checked elsewhere        
+        self.angleObject1.angle = 25
+        self.assert_(self.angleObject1.getDegrees() == 25, "Your method must be broken")
+        
+        self.angleObject1.angle = 25.8        
+        self.assert_(self.angleObject1.getDegrees() == 25.8, "Your method must be broken")
+        
+        pass
+    
+    def test106_020_getDegreesTestFail(self):
+        self.assert_(self.angleObject1.getDegrees() == 0, "Your method must be broken")
+        
+        # Setting angle without using the method is for testing purposes, as those methods are checked elsewhere        
+        self.angleObject1.angle = 25
+        self.assert_(self.angleObject1.getDegrees() == 25, "Your method must be broken")
+        
+        self.angleObject1.angle = 25.8        
+        self.assert_(self.angleObject1.getDegrees() == 25.8, "Your method must be broken")
+        
         pass
 
 if __name__ == "__main__":
